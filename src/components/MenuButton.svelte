e280062076df2a3ebb2f8157968ffc3c5a72d322
brenomaluf <brenomalufcontato@gmail.com>
2024-07-05 12:35:31 -0300

    favicon

--- src/components/MenuButton.svelte
+++ src/components/MenuButton.svelte
@@ -1,3 +1,5 @@
 <li>
-<a href="https://wa.me/5517996191384" class="border-2 rounded-xl px-5 h-10 p-2 text-white">Contato</a>
+	<a href="https://wa.me/5517996191384" class="border-2 rounded-xl px-5 h-10 p-2 text-white"
+		>Contato</a
+	>
 </li>
